00b7227ba74070c35924ba9fc9900dc3966c09a6
Obaid Farooqui <ofarooqui12crh@gmail.com>
2021-01-31 02:02:55 -0800

    Bismillah

---
+++ newpage.js
@@ -0,0 +1,429 @@
+
+ 	var asmaaAlHusna = [
+ 		{
+ 			"English": "God",
+ 			"Arabic": "الله‎"
+ 		},
+		{
+ 			"English": "The Most Merciful",
+ 			"Arabic": "ٱلْرَّحْمَـانُ"
+ 		},
+ 		{
+ 			"English": "The Bestower of Mercy",
+ 			"Arabic": "ٱلْرَّحِيْمُ"
+ 		},
+ 		{
+ 			"English": "The King and Owner of Dominion",
+ 			"Arabic": "ٱلْمَلِكُ"
+ 		},
+ 		{
+ 			"English": "The Absolutely Pure",
+ 			"Arabic": "ٱلْقُدُّوسُ"
+ 		},
+		{
+ 			"English": "The Source of Peace",
+ 			"Arabic": "ٱلْسَّلَامُ"
+ 		},
+		{
+ 			"English": "The Inspirer of Faith",
+ 			"Arabic": "ٱلْمُؤْمِنُ"
+ 		},
+		{
+ 			"English": "The Guardian",
+ 			"Arabic": "ٱلْمُهَيْمِنُ"
+ 		},
+ 		{
+ 			"English": "The Almighty",
+ 			"Arabic": "ٱلْعَزِيزُ"
+ 		},
+ 		{
+ 			"English": "The Compeller",
+ 			"Arabic": "ٱلْجَبَّارُ"
+ 		},
+ 		{
+ 			"English": "The Supreme",
+ 			"Arabic": "ٱلْمُتَكَبِّرُ"
+ 		},
+ 		{
+ 			"English": "The Creator",
+ 			"Arabic": "ٱلْخَالِقُ"
+ 		},
+ 		{
+ 			"English": "The Originator",
+ 			"Arabic": "ٱلْبَارِئُ"
+ 		},
+ 		{
+ 			"English": "The Shaper",
+ 			"Arabic": "ٱلْمُصَوِّرُ"
+ 		},
+ 		{
+ 			"English": "The Forgiving",
+ 			"Arabic": "ٱلْغَفَّارُ"
+ 		},
+ 		{
+ 			"English": "The Subduer",
+ 			"Arabic": "ٱلْقَهَّارُ"
+ 		},
+ 		{
+ 			"English": "The Giver",
+ 			"Arabic": "ٱلْوَهَّابُ"
+ 		},
+ 		{
+ 			"English": "The Provider",
+ 			"Arabic": "ٱلْرَّزَّاقُ"
+ 		},
+ 		{
+ 			"English": "The Opener",
+ 			"Arabic": "ٱلْفَتَّاحُ"
+ 		},
+ 		{
+ 			"English": "The All-Knowing",
+ 			"Arabic": "ٱلْعَلِيمُ"
+ 		},
+ 		{
+ 			"English": "The Withholder",
+ 			"Arabic": "ٱلْقَابِضُ"
+ 		},
+ 		{
+ 			"English": "The Extender",
+ 			"Arabic": "ٱلْبَاسِطُ"
+ 		},
+ 		{
+ 			"English": "The Abaser",
+ 			"Arabic": "ٱلْخَافِضُ"
+ 		},
+ 		{
+ 			"English": "The One Who Elevates",
+ 			"Arabic": "ٱلْرَّافِعُ"
+ 		},
+ 		{
+ 			"English": "The Honorer",
+ 			"Arabic": "ٱلْمُعِزُّ"
+ 		},
+ 		{
+ 			"English": "The Humiliator",
+ 			"Arabic": "ٱلْمُذِلُّ"
+ 		},
+ 		{
+ 			"English": "The All-Hearing",
+ 			"Arabic": "ٱلْسَّمِيعُ"
+ 		},
+ 		{
+ 			"English": "The All-Seeing",
+ 			"Arabic": "ٱلْبَصِيرُ"
+ 		},
+ 		{
+ 			"English": "The Impartial Judge",
+ 			"Arabic": "ٱلْحَكَمُ"
+ 		},
+ 		{
+ 			"English": "The Embodiment of Justice",
+ 			"Arabic": "ٱلْعَدْلُ"
+ 		},
+ 		{
+ 			"English": "The Subtle One",
+ 			"Arabic": "ٱلْلَّطِيفُ"
+ 		},
+ 		{
+ 			"English": "The All-Aware",
+ 			"Arabic": "ٱلْخَبِيرُ"
+ 		},
+ 		{
+ 			"English": "The Most Forbearing",
+ 			"Arabic": "ٱلْحَلِيمُ"
+ 		},
+ 		{
+ 			"English": "The Magnificent",
+ 			"Arabic": "ٱلْعَظِيمُ"
+ 		},
+ 		{
+ 			"English": "The Exceedingly Forgiving",
+ 			"Arabic": "ٱلْغَفُورُ"
+ 		},
+ 		{
+ 			"English": "The Most Appreciative",
+ 			"Arabic": "ٱلْشَّكُورُ"
+ 		},
+ 		{
+ 			"English": "The Most High",
+ 			"Arabic": "ٱلْعَلِيُّ"
+ 		},
+ 		{
+ 			"English": "The Greatest",
+ 			"Arabic": "ٱلْكَبِيرُ"
+ 		},
+ 		{
+ 			"English": "The Preserver",
+ 			"Arabic": "ٱلْحَفِيظُ"
+ 		},
+ 		{
+ 			"English": "The Sustainer",
+ 			"Arabic": "ٱلْمُقِيتُ"
+ 		},
+ 		{
+ 			"English": "The Reckoner",
+ 			"Arabic": "ٱلْحَسِيبُ"
+ 		},
+ 		{
+ 			"English": "The Majestic",
+ 			"Arabic": "ٱلْجَلِيلُ"
+ 		},
+ 		{
+ 			"English": "The Most Generous",
+ 			"Arabic": "ٱلْكَرِيمُ"
+ 		},
+ 		{
+ 			"English": "The Watchful One",
+ 			"Arabic": "ٱلْرَّقِيبُ"
+ 		},
+ 		{
+ 			"English": "The One Who Responds",
+ 			"Arabic": "ٱلْمُجِيبُ"
+ 		},
+ 		{
+ 			"English": "The Boundless",
+ 			"Arabic": "ٱلْوَاسِعُ"
+ 		},
+ 		{
+ 			"English": "The Most Wise",
+ 			"Arabic": "ٱلْحَكِيمُ"
+ 		},
+ 		{
+ 			"English": "The Most Loving",
+ 			"Arabic": "ٱلْوَدُودُ"
+ 		},
+ 		{
+ 			"English": "The Most Glorious",
+ 			"Arabic": "ٱلْمَجِيدُ"
+ 		},
+ 		{
+ 			"English": "The Resurrector",
+ 			"Arabic": "ٱلْبَاعِثُ"
+ 		},
+ 		{
+ 			"English": "The All-Observing Witness",
+ 			"Arabic": "ٱلْشَّهِيدُ"
+ 		},
+ 		{
+ 			"English": "The Truth",
+ 			"Arabic": "ٱلْحَقُّ"
+ 		},
+ 		{
+ 			"English": "The Trustee",
+ 			"Arabic": "ٱلْوَكِيلُ"
+ 		},
+ 		{
+ 			"English": "The Strongest",
+ 			"Arabic": "ٱلْقَوِيُّ"
+ 		},
+ 		{
+ 			"English": "The Firm One",
+ 			"Arabic": "ٱلْمَتِينُ"
+ 		},
+ 		{
+ 			"English": "The Protecting Associate",
+ 			"Arabic": "ٱلْوَلِيُّ"
+ 		},
+ 		{
+ 			"English": "The Praised One",
+ 			"Arabic": "ٱلْحَمِيدُ"
+ 		},
+ 		{
+ 			"English": "The Appraiser",
+ 			"Arabic": "ٱلْمُحْصِيُ"
+ 		},
+ 		{
+ 			"English": "The Initiator",
+ 			"Arabic": "ٱلْمُبْدِئُ"
+ 		},
+ 		{
+ 			"English": "The Restorer",
+ 			"Arabic": "ٱلْمُعِيدُ"
+ 		},
+ 		{
+ 			"English": "The Giver of Life",
+ 			"Arabic": "ٱلْمُحْيِى"
+ 		},
+ 		{
+ 			"English": "The Taker of Life",
+ 			"Arabic": "ٱلْمُمِيتُ"
+ 		},
+ 		{
+ 			"English": "The Ever-Living",
+ 			"Arabic": "ٱلْحَىُّ"
+ 		},
+ 		{
+ 			"English": "The Self-Subsisting",
+ 			"Arabic": "ٱلْقَيُّومُ"
+ 		},
+ 		{
+ 			"English": "The Finder",
+ 			"Arabic": "ٱلْوَاجِدُ"
+ 		},
+ 		{
+ 			"English": "The Illustrious",
+ 			"Arabic": "ٱلْمَاجِدُ"
+ 		},
+ 		{
+ 			"English": "The One",
+ 			"Arabic": "ٱلْوَاحِدُ"
+ 		},
+ 		{
+ 			"English": "The Only One",
+ 			"Arabic": "ٱلْأَحَد"
+ 		},
+ 		{
+ 			"English": "The Satisfier of All Needs",
+ 			"Arabic": "ٱلْصَّمَدُ"
+ 		},
+ 		{
+ 			"English": "The Most Powerful",
+ 			"Arabic": "ٱلْقَادِرُ"
+ 		},
+ 		{
+ 			"English": "The Creator of All Power",
+ 			"Arabic": "ٱلْمُقْتَدِرُ"
+ 		},
+ 		{
+ 			"English": "The Expediter",
+ 			"Arabic": "ٱلْمُقَدِّمُ"
+ 		},
+ 		{
+ 			"English": "The Delayer",
+ 			"Arabic": "ٱلْمُؤَخِّرُ"
+ 		},
+ 		{
+ 			"English": "The First",
+ 			"Arabic": "ٱلأَوَّلُ"
+ 		},
+ 		{
+ 			"English": "The Last",
+ 			"Arabic": "ٱلْآخِرُ"
+ 		},
+ 		{
+ 			"English": "ٱلْظَّاهِرُ",
+ 			"Arabic": "The Manifest One"
+ 		},
+ 		{
+ 			"English": "The Hidden One",
+ 			"Arabic": "ٱلْبَاطِنُ"
+ 		},
+ 		{
+ 			"English": "The Patron",
+ 			"Arabic": "ٱلْوَالِي"
+ 		},
+ 		{
+ 			"English": "The Exalted One",
+ 			"Arabic": "ٱلْمُتَعَالِي"
+ 		},
+ 		{
+ 			"English": "The Source of Goodness",
+ 			"Arabic": "ٱلْبَرُّ"
+ 		},
+ 		{
+ 			"English": "The Ever-Acceptor of Repentance",
+ 			"Arabic": "ٱلْتَّوَّابُ"
+ 		},
+ 		{
+ 			"English": "The Avenger",
+ 			"Arabic": "ٱلْمُنْتَقِمُ"
+ 		},
+ 		{
+ 			"English": "The Pardoner",
+ 			"Arabic": "ٱلْعَفُوُّ"
+ 		},
+ 		{
+ 			"English": "The Most Kind",
+ 			"Arabic": "ٱلْرَّؤُفُ"
+ 		},
+ 		{
+ 			"English": "The Owner of All",
+ 			"Arabic": "مَالِكُ ٱلْمُلْكُ	"
+ 		},
+ 		{
+ 			"English": "The Possessor of Majesty and Honor",
+ 			"Arabic": "ذُو ٱلْجَلَالِ وَٱلْإِكْرَامُ"
+ 		},
+ 		{
+ 			"English": "The Equitable One",
+ 			"Arabic": "ٱلْمُقْسِطُ"
+ 		},
+ 		{
+ 			"English": "The Gatherer",
+ 			"Arabic": "ٱلْجَامِعُ"
+ 		},
+ 		{
+ 			"English": "The Wealthy One",
+ 			"Arabic": "ٱلْغَنيُّ"
+ 		},
+ 		{
+ 			"English": "The Enricher",
+ 			"Arabic": "ٱلْمُغْنِيُّ"
+ 		},
+ 		{
+ 			"English": "The Preventer",
+ 			"Arabic": "ٱلْمَانِعُ"
+ 		},
+ 		{
+ 			"English": "The Distresser",
+ 			"Arabic": "ٱلْضَّارُ"
+ 		},
+ 		{
+ 			"English": "The Benefactor",
+ 			"Arabic": "ٱلْنَّافِعُ"
+ 		},
+ 		{
+ 			"English": "The Light",
+ 			"Arabic": "ٱلْنُّورُ"
+ 		},
+ 		{
+ 			"English": "The Guide",
+ 			"Arabic": "ٱلْهَادِي"
+ 		},
+ 		{
+ 			"English": "The Incomparable Originator",
+ 			"Arabic": "ٱلْبَدِيعُ"
+ 		},
+ 		{
+ 			"English": "The Everlasting",
+ 			"Arabic": "ٱلْبَاقِي"
+ 		},
+ 		{
+ 			"English": "The Inheritor of All",
+ 			"Arabic": "ٱلْوَارِثُ"
+ 		},
+ 		{
+ 			"English": "The Righteous Teacher",
+ 			"Arabic": "ٱلْرَّشِيدُ"
+ 		},
+ 		{
+ 			"English": "The Patient One",
+ 			"Arabic": "ٱلْصَّبُورُ"
+ 		}
+ 	];	
+	
+	var num = getRandomInt(0, 99); 
+
+	var arabicName = document.createElement('div');
+	arabicName.className = "arabicName";
+	arabicName.textContent = asmaaAlHusna[num].Arabic;
+	document.getElementsByClassName("parent")[0].appendChild(arabicName);
+
+	var translation = document.createElement('div');
+	translation.className = "translation";
+	translation.textContent = asmaaAlHusna[num].English;
+	document.getElementsByClassName("parent")[0].appendChild(translation);
+
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
